4af7fa4da767b801bdbaf86ba737da9474681150
Maksym Chertenkov <max.chertenkov@gmail.com>
2016-05-23 17:18:58 +0300

    add list line

--- main.js
+++ main.js
@@ -3,8 +3,24 @@
 document.addEventListener("submit",  function getContents() {
 		event.preventDefault();
 		let input = document.querySelector("input[type=text]")
-
+		appendListLine() 
+		
 		input.value = ""
 })
 
-
+function appendListLine() {
+	let list = document.querySelector("ul")
+	list.removeAttribute("hidden")
+	var li = document.createElement("li");
+	var p = document.createElement("p");
+	var btn1 = document.createElement("button");
+	var btn2 = document.createElement("button");
+	list.appendChild(li)
+	li.appendChild(p)
+	li.appendChild(btn1)
+	li.appendChild(btn2)
+	btn1.classList.add("complete")
+	btn1.innerHTML = "&#10003;"
+	btn2.classList.add("delete")
+	btn2.innerHTML = "&#10006;"
+}
