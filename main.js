84e1b5a75904a79169705c5e5ffa111524fecc94
Maksym Chertenkov <max.chertenkov@gmail.com>
2016-05-23 17:18:21 +0300

    get input content

---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict"
+
+document.addEventListener("submit",  function getContents() {
+		event.preventDefault();
+		let input = document.querySelector("input[type=text]")
+
+		input.value = ""
+})
+
+
